33e34eba7b21e58b42192ecf842c844cadd0a7c8
Mateo Olaya Bernal <pub.mateo.olaya@gmail.com>
2026-04-02 21:12:43 -0400

    Rename Sources/skills to Sources/AgentManager

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser")
             ],
-            path: "Sources/skills"
+            path: "Sources/AgentManager"
         )
     ]
 )
